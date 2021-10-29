c432f0e2a5868705668e11ad1fc593f9101837ce
eltonbrayner <eltonbrayner1@gmail.com>
2021-10-28 23:13:17 -0300

    🚀  Finish challenge

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,15 +9,22 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    if (!newTaskTitle) return;
+    setTasks((oldState) => [
+      ...oldState,
+      { id: Math.random(), title: newTaskTitle, done: false },
+    ]);
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+    const newTasks = [...tasks];
+    newTasks.find((task) => task.id === id && (task.done = !task.done));
+    setTasks(newTasks);
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    const newTasks = tasks.filter((task) => task.id !== id);
+    setTasks(newTasks);
   }
 
   return (
@@ -26,18 +33,18 @@ export function Home() {
 
       <TodoInput addTask={handleAddTask} />
 
-      <TasksList 
-        tasks={tasks} 
+      <TasksList
+        tasks={tasks}
         toggleTaskDone={handleToggleTaskDone}
-        removeTask={handleRemoveTask} 
+        removeTask={handleRemoveTask}
       />
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#EBEBEB'
-  }
-})
+    backgroundColor: '#EBEBEB',
+  },
+});
